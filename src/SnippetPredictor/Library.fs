163160e69b9123f2762f5837b0d408bac8c435c3
krymtkts <takatoshi.kuriyama@gmail.com>
2025-02-10 13:42:22 +0900

    Implement empty predictor.

--- src/SnippetPredictor/Library.fs
+++ src/SnippetPredictor/Library.fs
@@ -1,5 +1,61 @@
 ﻿namespace SnippetPredictor
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System
+open System.Collections.Generic
+open System.Management.Automation
+open System.Management.Automation.Subsystem
+open System.Management.Automation.Subsystem.Prediction
+open System.Threading
+
+type SamplePredictor(guid: string) =
+    let id = Guid.Parse(guid)
+
+    [<Literal>]
+    let name = "SnippetPredictor"
+
+    [<Literal>]
+    let description = "A predictor that suggests a snippet based on the input."
+
+    interface ICommandPredictor with
+        member __.Id = id
+        member __.Name = name
+        member __.Description: string = description
+        member __.FunctionsToDefine: Dictionary<string, string> = Dictionary<string, string>()
+
+        member __.GetSuggestion
+            (
+                client: PredictionClient,
+                context: PredictionContext,
+                cancellationToken: CancellationToken
+            ) : SuggestionPackage =
+            let input = context.InputAst.Extent.Text
+
+            if String.IsNullOrWhiteSpace(input) then
+                // NOTE: cannot pass null.
+                SuggestionPackage(List<PredictiveSuggestion>([]))
+            else
+                SuggestionPackage(List<PredictiveSuggestion>([ PredictiveSuggestion(sprintf "%s SNIPPET" input) ]))
+
+        member __.CanAcceptFeedback(client: PredictionClient, feedback: PredictorFeedbackKind) : bool = false
+
+        member __.OnSuggestionDisplayed(client: PredictionClient, session: uint32, countOrIndex: int) : unit = ()
+
+        member __.OnSuggestionAccepted(client: PredictionClient, session: uint32, acceptedSuggestion: string) : unit =
+            ()
+
+        member __.OnCommandLineAccepted(client: PredictionClient, history: IReadOnlyList<string>) : unit = ()
+
+        member __.OnCommandLineExecuted(client: PredictionClient, commandLine: string, success: bool) : unit = ()
+
+type Init() =
+    [<Literal>]
+    let identifier = "f6dbcf05-2f90-4c47-b40e-6a4cec337cc1"
+
+    interface IModuleAssemblyInitializer with
+        member __.OnImport() =
+            let predictor = SamplePredictor(identifier)
+            SubsystemManager.RegisterSubsystem(SubsystemKind.CommandPredictor, predictor)
+
+    interface IModuleAssemblyCleanup with
+        member __.OnRemove(psModuleInfo: PSModuleInfo) =
+            SubsystemManager.UnregisterSubsystem(SubsystemKind.CommandPredictor, Guid(identifier))
