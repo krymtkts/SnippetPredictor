4474ffc78ba1e8a2c0bdf76ec761217d676096cd
krymtkts <takatoshi.kuriyama@gmail.com>
2025-02-11 19:19:51 +0900

    Add psakefile for build and release automation.

---
+++ psakefile.ps1
@@ -0,0 +1,89 @@
+Properties {
+    if (-not $Stage) {
+        $Stage = 'Debug'
+    }
+    if ($DryRun -eq $null) {
+        $DryRun = $true
+    }
+    $ModuleName = Get-ChildItem ./src/*/*.psd1 | Select-Object -ExpandProperty BaseName
+    $ModuleVersion = (Resolve-Path "./src/${ModuleName}/*.fsproj" | Select-Xml '//Version/text()').Node.Value
+    $ModuleSrcPath = Resolve-Path "./src/${ModuleName}/"
+    $ModulePublishPath = Resolve-Path "./bin/${ModuleName}/"
+    "Module: ${ModuleName} ver${ModuleVersion} root=${ModuleSrcPath} publish=${ModulePublishPath}"
+}
+
+Task default -Depends TestAll
+
+Task Init {
+    'Init is running!'
+    dotnet tool restore
+}
+
+Task Clean {
+    'Clean is running!'
+    Get-Module pocof -All | Remove-Module -Force -ErrorAction SilentlyContinue
+    @(
+        "./src/*/*/${Stage}"
+        './release'
+        "${ModulePublishPath}/*"
+    ) | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue -Exclude .gitkeep
+}
+
+
+Task Lint {
+    dotnet fantomas ./src --check
+    if (-not $?) {
+        throw 'dotnet fantomas failed.'
+    }
+    $warn = Invoke-ScriptAnalyzer -Path .\psakefile.ps1 -Settings .\PSScriptAnalyzerSettings.psd1
+    if ($warn) {
+        throw 'Invoke-ScriptAnalyzer for psakefile.ps1 failed.'
+    }
+}
+
+Task Build -Depends Clean {
+    'Build command let!'
+    Import-LocalizedData -BindingVariable module -BaseDirectory $ModuleSrcPath -FileName "${ModuleName}.psd1"
+    if ($module.ModuleVersion -ne (Resolve-Path "./src/*/${ModuleName}.fsproj" | Select-Xml '//Version/text()').Node.Value) {
+        throw 'Module manifest (.psd1) version does not match project (.fsproj) version.'
+    }
+    dotnet publish -c $Stage
+    "Completed to build $ModuleName ver$ModuleVersion"
+}
+
+Task Import -Depends Build {
+    "Import $ModuleName ver$ModuleVersion"
+    if ( -not ($ModuleName -and $ModuleVersion)) {
+        throw "ModuleName or ModuleVersion not defined. $ModuleName, $ModuleVersion"
+    }
+    switch ($Stage) {
+        'Debug' {
+            Import-Module (Resolve-Path "${ModuleSrcPath}/bin/Debug/*/publish/*.psd1") -Global
+        }
+        'Release' {
+            Import-Module "./bin/$ModuleName" -Global
+        }
+    }
+}
+
+Task Release -PreCondition { $Stage -eq 'Release' } -Depends Import {
+    "Release $($ModuleName)! version=$ModuleVersion dryrun=$DryRun"
+
+    $m = Get-Module $ModuleName
+    if ($m.Version -ne $ModuleVersion) {
+        throw "Version inconsistency between project and module. $($m.Version), $ModuleVersion"
+    }
+    $p = Get-ChildItem "${ModulePublishPath}/*.psd1"
+    if (-not $p) {
+        throw "Module manifest not found. $($m.ModuleBase)/*.psd1"
+    }
+
+    $Params = @{
+        Path = $p.FullName
+        Repository = 'PSGallery'
+        ApiKey = (Get-Credential API-key -Message 'Enter your API key as the password').GetNetworkCredential().Password
+        WhatIf = $DryRun
+        Verbose = $true
+    }
+    Publish-PSResource @Params
+}
