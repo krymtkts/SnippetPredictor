af3cdec32fabc5e13cf8c72927f1e1859455c98a
krymtkts <takatoshi.kuriyama@gmail.com>
2025-02-15 23:11:22 +0900

    Separate `Snippet` module from `Library.fs`.

---
+++ src/SnippetPredictor/Snippet.fs
@@ -0,0 +1,58 @@
+﻿module Snippet
+
+open System
+open System.IO
+open System.Collections
+open System.Management.Automation.Subsystem.Prediction
+open System.Text.Json
+open System.Threading
+
+type SnippetEntry = { snippet: string; tooltip: string }
+type Snippets = { snippets: SnippetEntry[] }
+
+[<Literal>]
+let snippetFilesName = ".snippet-predictor.json"
+
+[<Literal>]
+let snippetSymbol = ":snp"
+
+let snippets = Concurrent.ConcurrentQueue<SnippetEntry>()
+
+let load () =
+    let snippetPath =
+        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), snippetFilesName)
+
+    let cancellationToken = new CancellationToken()
+
+    if File.Exists(snippetPath) then
+        task {
+            let! json = snippetPath |> File.ReadAllTextAsync
+
+            json
+            |> JsonSerializer.Deserialize<Snippets>
+            |> function
+                | null -> Array.empty
+                | _ as snippets -> snippets.snippets
+            |> Array.iter snippets.Enqueue
+        }
+        |> _.WaitAsync(cancellationToken)
+        |> ignore
+
+let getFilter (input: string) =
+    // NOTE: Remove the snippet symbol from the input.
+    // NOTE: Snippet symbol is used to exclude other predictors from suggestions.
+    input.Replace(snippetSymbol, "")
+
+let getSnippets (filter: string) : SnippetEntry seq =
+    snippets |> Seq.filter _.snippet.Contains(filter)
+
+let snippetToTuple (s: SnippetEntry) = s.snippet, s.tooltip
+
+let getPredictiveSuggestions (input: string) : Generic.List<PredictiveSuggestion> =
+    if String.IsNullOrWhiteSpace(input) then
+        Seq.empty
+    else
+        input
+        |> (getFilter >> getSnippets)
+        |> Seq.map (snippetToTuple >> PredictiveSuggestion)
+    |> Linq.Enumerable.ToList
