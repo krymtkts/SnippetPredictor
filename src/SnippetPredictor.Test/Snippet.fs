de79fafaff97b5270c8ecece6d3bef2d36041a8e
krymtkts <takatoshi.kuriyama@gmail.com>
2025-03-08 20:43:30 +0900

    Add test cases for snippet loading with null and valid configurations.

--- src/SnippetPredictor.Test/Snippet.fs
+++ src/SnippetPredictor.Test/Snippet.fs
@@ -170,9 +170,13 @@ module getPredictiveSuggestions =
                       actual.SuggestionText = expected.Snippet && actual.ToolTip = expected.Tooltip)
               }
 
-              test "when " {
+              test "when no snippets match" {
                   Snippet.getPredictiveSuggestions "    exo    "
                   |> Expect.isEmpty "should return empty."
+              }
+
+              test "when input is whitespace" {
+                  Snippet.getPredictiveSuggestions "    " |> Expect.isEmpty "should return empty."
               }
 
               ]
@@ -201,12 +205,32 @@ let tests_loadSnippets =
                   "'An error occurred while parsing .snippet-predictor.json': Expected depth to be zero at the end of the JSON payload. There is an open JSON object or array that should be closed. Path: $.Snippets[0] | LineNumber: 1 | BytePositionInLine: 15."
           }
 
-          test "when snippet file is valid and snippets is null" {
+          test "when snippet file is valid and null" {
               Snippet.loadSnippets (fun () -> "./", "./.snippet-predictor-null.json")
               |> function
                   | Ok _ -> failtest "Expected Error but got Ok."
                   | Error e -> e
               |> Expect.equal "should return Error entry" "'.snippet-predictor.json is null or invalid format.'"
+          }
+
+          test "when snippet file is valid and snippets is null" {
+              Snippet.loadSnippets (fun () -> "./", "./.snippet-predictor-snippet-null.json")
+              |> function
+                  | Ok s -> s
+                  | Error e -> failtest $"Expected Error but got Error. {e}"
+              |> Expect.isEmpty "should return Empty"
+          }
+
+          test "when snippet file is valid" {
+              let expected =
+                  [| { SnippetEntry.Snippet = "echo 'example'"
+                       SnippetEntry.Tooltip = "example tooltip" } |]
+
+              Snippet.loadSnippets (fun () -> "./", "./.snippet-predictor-valid.json")
+              |> function
+                  | Ok s -> s
+                  | Error e -> failtest $"Expected Error but got Error. {e}"
+              |> Expect.equal "should return snippets" expected
           }
 
           ]
