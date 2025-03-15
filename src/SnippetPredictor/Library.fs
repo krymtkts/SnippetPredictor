2652241678cc77f0f0860a1e2d7f158c5a885400
krymtkts <takatoshi.kuriyama@gmail.com>
2025-03-15 14:16:35 +0900

    Restrict allowed characters for group parameter.

--- src/SnippetPredictor/Library.fs
+++ src/SnippetPredictor/Library.fs
@@ -91,6 +91,7 @@ type AddSnippetCommand() =
                 Mandatory = false,
                 ValueFromPipelineByPropertyName = true,
                 HelpMessage = "The group of the snippet")>]
+    [<ValidatePattern("^[A-Za-z0-9]+$")>]
     member val Group: string | null = null with get, set
 
     override __.ProcessRecord() =
