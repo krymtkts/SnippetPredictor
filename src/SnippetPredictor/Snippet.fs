a8dbfa3c0ce4d2b311affb5ad724a18b90a66cfa
krymtkts <takatoshi.kuriyama@gmail.com>
2026-01-03 17:49:36 +0900

    Extract `Debug` module for enhanced maintainability.

--- src/SnippetPredictor/Snippet.fs
+++ src/SnippetPredictor/Snippet.fs
@@ -6,43 +6,6 @@ open System.Text.Json
 open System.Text.Json.Serialization
 open System.Text.RegularExpressions
 
-#if DEBUG
-[<AutoOpen>]
-module Debug =
-    open System.Runtime.CompilerServices
-    open System.Runtime.InteropServices
-
-    let lockObj = new obj ()
-
-    [<Literal>]
-    let logPath = "./debug.log"
-
-    [<AbstractClass; Sealed>]
-    type Logger =
-        static member LogFile
-            (
-                res,
-                [<Optional; DefaultParameterValue(""); CallerMemberName>] caller: string,
-                [<CallerFilePath; Optional; DefaultParameterValue("")>] path: string,
-                [<CallerLineNumber; Optional; DefaultParameterValue(0)>] line: int
-            ) =
-
-            // NOTE: lock to avoid another process error when dotnet test.
-            lock lockObj (fun () ->
-                use sw = new StreamWriter(logPath, true)
-
-                res
-                |> List.iter (
-                    fprintfn
-                        sw
-                        "[%s] %s at %d %s <%A>"
-                        (DateTimeOffset.Now.ToString("yyyy-MM-dd'T'HH:mm:ss.fffzzz"))
-                        path
-                        line
-                        caller
-                ))
-#endif
-
 module Nullable =
     let dispose (d: 'a | null when 'a :> IDisposable) =
         d
